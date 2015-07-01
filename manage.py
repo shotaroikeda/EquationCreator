ed2263a2428e20cc0c4508341ed04c04f8c554e2
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-07-01 22:07:32 +0900

    Added functions up to being able to determine which genes to reproduce from

--- manage.py
+++ manage.py
@@ -1,12 +1,39 @@
-from gene import *
+import gene
+import numpy as np
+
 
 CHANCE_UNFIT = 0.0000001  # The default fitness a completely bad gene has
 
 def initialize():
-    return [generate() for i in range(0, 20)]
+    return [gene.generate() for i in range(0, 20)]
 
-def generate_fitness(gene_array, initial_num = 42):
-    return [float(5.0 / (5.0 + abs(initial_num - eval(evaluate(g, True))))) for g in gene_array]
+def generate_fitness(gene_array, initial_num=42):
+    return [float((1.0 / (1.0 +
+                          abs(initial_num - gene.evaluate(g, True)))))
+            for g in gene_array]
+
+def superfast():
+    a = initialize()
+    b = generate_fitness(a)
+    return sorted(list(zip(a, b)), key=lambda x : x[1])
 
 def test_roll():
     return generate_fitness(initialize())
+
+def get_probability(num, total):
+    return float(num / total)
+
+def weighted_choice(gene_array, fitness_array):
+    fix = np.vectorize(get_probability)
+    draw1, draw2 = np.random.choice(gene_array,
+                                    2,
+                                    replace=False,
+                                    p=fix(fitness_array, sum(fitness_array)))
+    return (draw1, draw2)
+
+def f():
+    a = initialize()
+    b = generate_fitness(a)
+
+    print weighted_choice(a, b)
+
