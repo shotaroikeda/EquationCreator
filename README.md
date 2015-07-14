ee35f516ccb5ef28a2b0088f0bf453b89c5a5ce4
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-07-15 06:20:47 +0900

    More documentation of code for understandability

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Finds a simple function given a number. (Something like 35->10*3+2-9/3)
 
 credit to http://www.ai-junkie.com/ga/intro/gat1.html for providing a thorough explanation of the Genetic Algorithm
+
+
--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import gene
 import os
 from sys import argv
 
+# Most of this is a way to make things pretty.
+
 P_VERSION = "1.0"
 
 FILE_OUT = "output.txt"
+
+# Default Settings: Change these to play with values, or use
+# the args on launch 
 MUTATION_RATE = 0.15
 REPRODUCTION_RATE = 0.7
 # MAX_GENE_POOL is set in manage.py <- bad design...
@@ -49,34 +54,43 @@ for index, arg in enumerate(argv):
 
 FINAL_NUMBER = 42
 
-#try:
-#    a = int(raw_input("Please Enter a Natural Number (0 <= n ) : "))
-#    
-#    if a < 0:
-#        raise ValueError("Invalid Input. Please Enter a number greater than 0")
-#
-#except ValueError:
-#    print "bad input, defaulting to 42"
+try:
+    a = int(raw_input("Please Enter a Natural Number (0 <= n ) : "))
+    
+    if a < 0:
+        raise ValueError("Invalid Input. Please Enter a number greater than 0")
 
-# TODO: set the userinput number to whatever they choose
+    FINAL_NUMBER = a
 
+except ValueError:
+    print "bad input, defaulting to 42"
+
+# Open file for verbose output
 f = open(FILE_OUT, "w")
+
+# Keep track of the generation
 generation = 0
 
+# Initial set up of the gene pool.
 gene_pool = manage.start()
 
+# On a really lucky solution, don't bother doing much...
 if manage.initial_scan(gene_pool):
     print "Found a solution!"
     print "That wasn't that interesting though...try again..."
     exit()
 
+# Keep track of the previous and max fit to prevent cluttering of the console.
 prev_gene = ""
 
 while not gene_pool[-1][1] == 1:
+# When the Max fitted gene pool is 1, this is when a solution is found.
+    # ALWAYS write to the file, but don't print to stdout
     f.write("\nGENERATION: %d\n" % (generation))
     for g, fit in gene_pool:
         f.write("%s  %1.8f  %s\n" % (g, fit, gene.show_eq(g)))
 
+    # If there is a new maximum, do the verbose output
     if not (prev_gene == gene_pool[-1][0]):
         os.system('clear')
         print "GENERATION: %d\n" % (generation)
@@ -85,6 +99,7 @@ while not gene_pool[-1][1] == 1:
         for g, fit in gene_pool:
             print "%s  %1.8f  %s" % (g, fit, gene.show_eq(g))
 
+    # Reproduce 2 genes and add the resulting ones to the list of genes
     ng1, ng2 = manage.reproduce(gene_pool, REPRODUCTION_RATE, MUTATION_RATE)
     manage.add(gene_pool, ng1, FINAL_NUMBER)
     manage.add(gene_pool, ng2, FINAL_NUMBER)
@@ -93,6 +108,7 @@ while not gene_pool[-1][1] == 1:
     prev_gene = gene_pool[-1][1]
 
 
+# Pretty print things when the genes have solved the problem
 f.write("\nGENERATION: %d\n" % (generation))
 for g, fit in gene_pool:
     f.write("%s  %1.8f  %s\n" % (g, fit, gene.show_eq(g)))
@@ -108,4 +124,3 @@ print "\t - Value: %d\n" % (gene.evaluate(gene_pool[-1][0]))
 for n, gfit in enumerate(gene_pool):
     g, fit = gfit
     print "%4d  %s  %1.8f  %s" % (n, g, fit, gene.show_eq(g))
-
